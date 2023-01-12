f55f0f8247aea4f70e8372eca67351d5e04ff08e
Kappaonly <matuskokrivda@gmail.com>
2023-01-12 18:27:17 +0100

    Add project files.

---
+++ TerminalWars/TerminalWars.cpp
@@ -0,0 +1,12 @@
+﻿// TerminalWars.cpp : Defines the entry point for the application.
+//
+
+#include "TerminalWars.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
